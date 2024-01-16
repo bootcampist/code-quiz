2b7f21049bb1d926d9038823ecddc9cdc9174d0c
Phoenix <bootcampgithub@gmail.com>
2024-01-16 13:13:11 +0000

    replace test questions with actual ones

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,32 +1,32 @@
 const questions = [{
     name: "question1",
-    question: "question1?",
-    answer: "answer1",
-    incorrect: ["incorrect1", "incorrect2","incorrect3"]
+    question: "How would a data index of 0 be set?",
+    answer: "Element.setAttribute(‘data-index’, 0);",
+    incorrect: ["Element.getAttribute(‘data-index’, 0);", "Element.dataset(‘data-index’, 0);","Element.innerIndex = 0;"]
 },
 {
     name: "question2",
-    question: "question2?",
-    answer: "answer2",
-    incorrect: ["incorrect1", "incorrect2","incorrect3"]
+    question: "What does the querySelectorAll() method return?",
+    answer: "A nodelist",
+    incorrect: ["An array", "A string","A stringray"]
 },
 {
     name: "question3",
-    question: "question3?",
-    answer: "answer3",
-    incorrect: ["incorrect1", "incorrect2","incorrect3"]
+    question: "How can an object be turned into a string to be stored in Local Storage?",
+    answer: "variable = JSON.stringify(object);",
+    incorrect: ["variable = object.stringForm();","variable = object.toString();", "variable = JSON.stringSet(object);"]
 },
 {
     name: "question4",
-    question: "question4?",
-    answer: "answer4",
-    incorrect: ["incorrect1", "incorrect2","incorrect3"]
+    question: "How might an object be stored in Local Storage?",
+    answer: "localStorage.setItem([‘item name’], [item]);",
+    incorrect: ["localStorage.setAttribute([‘attribute name’], [attribute]);", "localStorage.setList([‘setlist name’], [setlist]);","localStorage.setString([‘string name’], [string]);"]
 },
 {
     name: "question5",
-    question: "question5?",
-    answer: "answer5",
-    incorrect: ["incorrect1", "incorrect2","incorrect3"]
+    question: "How can items in an array be sorted in descending order by numerical value?",
+    answer: "array.sort((a,b)=>{return b-a});",
+    incorrect: ["array.sort((a,b)=>{return a-b});", "array.sort(a,b, return b-a);","array.sort(a,b, return a-b);"]
 }];
 
 //Global Variables
@@ -51,7 +51,7 @@ let randomAnswers = [];
 let correct;
 let text;
 let score = 0;
-let time = 60;
+let time = 75;
 let complete = false;
 const user = {name: "",
             score: 0           
@@ -194,3 +194,7 @@ start.addEventListener('click', (e)=>{
     window.open('./highscores.html', target='_self');
  });
 
+
+
+
+ 
